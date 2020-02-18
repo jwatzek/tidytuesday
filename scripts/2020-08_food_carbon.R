9b5df6cf9977729470a8249731b2375003b10d7e
Julia Watzek <j.watzek@gmail.com>
2020-02-18 13:09:21 -0500

    swap color palette to more cvd friendly

--- scripts/2020-08_food_carbon.R
+++ scripts/2020-08_food_carbon.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(ggtext)
+library(viridis)
 
 d = read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-02-18/food_consumption.csv')
 # write_csv(d, 'data/2020-08_food_carbon.csv')
@@ -15,6 +16,11 @@ d3 = d2 %>%
     arrange(product_cat, desc(co2_emmission)) %>% 
     mutate(country = '**Average**', panel = 1)
 
+d2 %>% 
+    group_by(food_category) %>% 
+    summarise(M = mean(co2_emmission/consumption, na.rm = T), SD = sd(co2_emmission/consumption, na.rm = T)) %>% 
+    arrange(desc(M))
+
 cat_order = d3$food_category
 
 d4 = d2 %>% 
@@ -29,18 +35,18 @@ d4 = d2 %>%
            food_category = factor(food_category, levels = rev(cat_order)),
            co2_emmission2 = ifelse(product_cat == 'Animal Products', co2_emmission, -co2_emmission))
 
-pal = wesanderson::wes_palette('Rushmore1', 20, type = 'continuous')[c(6:8, 10, 14:20)]
+pal = c(viridis(4, end = .8), inferno(7, begin = .4, direction = -1))
 
 p = ggplot(d4, aes(country, co2_emmission2)) +
     lemon::facet_rep_grid(panel ~ ., scales = 'free_y', space = 'free_y', repeat.tick.labels = T) +
     geom_bar(aes(fill = food_category), stat = 'identity', width = .8) +
     coord_flip() +
-    scale_y_continuous(expand = c(.05, .1)) +
+    scale_y_continuous(expand = c(.05, .1), limits = c(-250, 2200), breaks = c(-250, seq(0, 2000, 500)), labels = c(250, seq(0, 2000, 500))) +
     scale_fill_manual('', values = pal[c(1:4, 11:5)], breaks = rev(cat_order)[c(1:4, 11:5)]) +
     scale_color_identity() +
     labs(x = '', y = '**CO<sub>2</sub> emission** [kg/person/year]',
          title = 'Are you on a low carb(on) diet?', 
-         subtitle = glue::glue("130 countries' CO<sub>2</sub> emission based on the amount of <b style='color: {pal[4]}'>non-animal products</b> and <b style='color: {pal[11]}'>animal products</b> that a country<br/>supplied for consumption in 2013. This may include a percentage of food that is not consumed, e.g. food waste."), 
+         subtitle = glue::glue("130 countries' CO<sub>2</sub> emission based on the amount of <b style='color: {pal[4]}'>non-animal products</b> and <b style='color: {pal[10]}'>animal products</b> that a country<br/>supplied for consumption in 2013. This may include a percentage of food that is not consumed, e.g. food waste."), 
          caption = 'Data by the Food and Agriculture Organization of the United Nations (via nu3)\n#TidyTuesday • @watzoever') +
     cowplot::theme_minimal_vgrid() +
     theme(text = element_text(family = 'Source Sans Pro'),
@@ -57,4 +63,16 @@ p = ggplot(d4, aes(country, co2_emmission2)) +
           plot.margin = unit(c(2, 6.5, 2, 2), 'cm'))
 
 ggsave('plots/2020-08_food_carbon.png', p, width = 8, height = 15, scale = 1.7)
+
+# make smaller plot to test compatability for people w color vision deficiencies
+d4a = d4 %>% 
+    mutate(rank = dense_rank(total_by_country)) %>%
+    filter(country == 'Average' | rank <= 20 | rank > max(rank) - 20)
+
+p2 = p %+% d4a + 
+    labs(x = '', y = '', title = '', subtitle = '', caption = '') +
+    theme_void() +
+    theme(strip.text.y = element_blank(), legend.position = 'none')
+
+ggsave('plots/2020-08_food_carbon_cvd.png', colorblindr::cvd_grid(p2), width = 8, height = 8, scale = 1.7)
 
